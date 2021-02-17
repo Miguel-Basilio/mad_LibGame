e586441bdb95668a71a3fd484c0ebfc8f2c4efb7
Miguel-Basilio <lokoangel06@gmail.com>
2021-02-16 21:23:15 -0800

    Initial Commit

---
+++ mad_LibGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mad_LibGame
+//
+//  Created by AB on 2/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
