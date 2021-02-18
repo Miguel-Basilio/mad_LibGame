5109080b411bcf9142f74a4d0e3f07e09854fe27
Miguel-Basilio <lokoangel06@gmail.com>
2021-02-17 16:02:02 -0800

    first commit small code where user inputs some data to fill out a history about fall. just to have a laugh.

--- mad_LibGame/main.cpp
+++ mad_LibGame/main.cpp
@@ -1,14 +1,55 @@
-//
-//  main.cpp
-//  mad_LibGame
-//
-//  Created by AB on 2/16/21.
-//
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+  
+  //takes user input to write a random history
+    string nounOne, nounTwo,nounThree, pluralNoun, pluralNounTwo,finalNoun;
+    string monthOne, monthTwo, adjective, verb_ed, cardinalNumber;
+    cout << "The Following is a history related to the fall season" << endl;
+    cout <<" Enter  a noun: "<< endl;
+    cin >> nounOne;
+    cout <<" Write a Cardinal Number:(no numbers) "<< endl;
+    cin >> cardinalNumber;
+    cout <<" Enter a month: "<< endl;
+    cin >> monthOne;
+    cout <<" Enter a different month: "<< endl;
+    cin >> monthTwo;
+    cout <<"Enter a different noun: "<< endl;
+    cin >> nounTwo;
+    cout <<"Enter an adjective: "<< endl;
+    cin >> adjective;
+    cout <<" Enter a noun: "<< endl;
+    cin >> nounThree;
+    cout <<" Enter a plural noun: "<< endl;
+    cin >> pluralNoun;
+    cout <<" Enter a different plural noun: "<< endl;
+    cin >> pluralNounTwo;
+    cout <<" Enter a verb: "<< endl;
+    cin >> verb_ed;
+    cout <<" Enter a different plural noun: "<< endl;
+    cin >> finalNoun;
+    
+    
+    cout << ""<< endl;
+    //outputs History
+    cout << "Fall, also know as "<< nounOne << ", is one of the\
+    "<<cardinalNumber<< " seasons of the year. In the United\
+    States, fall takes place from "<< monthOne << " to "<<monthTwo<<".\
+    Two holidays that happen during fall are "<<nounTwo<<", where people\
+    dress up in "<<adjective<< " costumes and\
+    “"<<nounThree << " Day”, a time to give thanks.\
+    This is also harvest time, when "<<pluralNoun<<"\
+    and "<<pluralNounTwo<<" are "<<verb_ed<< " for us to\
+        eat fresh or bake into " <<finalNoun<<""<< endl;
+    
+    
+    
+    
+    
+    
     return 0;
 }
